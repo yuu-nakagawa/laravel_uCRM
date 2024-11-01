e6f5971ff91f3fd71c252be173112aafce362bbe
yuu-nakagawa <81138327+yuu-nakagawa@users.noreply.github.com>
2024-11-01 13:52:14 +0900

    customerSearch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Purchase;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            UserSeeder::class,
+            ItemSeeder::class,
+        ]);
+        
         \App\Models\Customer::factory(1000)->create();
+
+        $items = \App\Models\Item::all();
+
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items){
+            $purchase->items()->attach(
+                $items->random(rand(1,3))->pluck('id')->toArray(),
+                ['quantity' => rand(1,5)]
+            );
+        });
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
@@ -22,9 +39,5 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([
-            UserSeeder::class,
-            ItemSeeder::class,
-        ]);
     }
 }
