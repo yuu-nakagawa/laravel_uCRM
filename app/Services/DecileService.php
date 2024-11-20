dc1607fc0cdd09ef08f254bfdff909fdae9e4846
yuu-nakagawa <81138327+yuu-nakagawa@users.noreply.github.com>
2024-11-20 11:27:42 +0900

    decile

--- app/Services/DecileService.php
+++ app/Services/DecileService.php
@@ -1,53 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Inertia\Inertia;
-use App\Models\Order;
+namespace App\Services;
 use Illuminate\Support\Facades\DB;
 
-class AnalysisController extends Controller
+class DecileService
 {
-    public function index()
+    public static function decile($subQuery)
     {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
-        // $period = Order::betweenDate($startDate, $endDate)
-        // ->groupBy('id')
-        // ->selectRaw('id, sum(subtotal) as total,
-        // customer_name, status, created_at')
-        // ->orderBy('created_at')
-        // ->paginate(50);
-
-        // dd($period);
-
-
-        // $subQuery = Order::betweenDate($startDate, $endDate)
-        // ->where('status', true)
-        // ->groupBy('id')
-        // ->selectRaw('id, sum(subtotal) as totalPerPurchase,
-        // DATE_FORMAT(created_at, "%Y%m%d") as date');
-
-        // $data = DB::table($subQuery)
-        // ->groupBy('date')
-        // ->selectRaw('date, sum(totalPerPurchase) as total')
-        // ->get();
-
-        // dd($data);
-
-        return Inertia::render('Analysis');
-    }
-
-    public function decile()
-    {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
         //購買IDごとにまとめる
-        $subQuery = Order::betweenDate($startDate, $endDate)
-        ->groupBy('id')
+        $subQuery = $subQuery->groupBy('id')
         ->selectRaw('id, customer_id, customer_name, 
         SUM(subtotal) as totalPerPurchase');
 
@@ -129,7 +90,9 @@ class AnalysisController extends Controller
             round(100*totalPerGroup/@total,1) as
             totalRatio')
         ->get();
+        $labels = $data->pluck('decile');
+        $totals = $data->pluck('totalPerGroup');
 
-        // dd($data);
+        return [$data, $labels, $totals];
     }
 }
--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -5,6 +5,7 @@ import { onMounted, reactive } from 'vue';
 import { getToday } from '@/common';
 import axios from 'axios';
 import Chart from '@/Components/Chart.vue';
+import ResultTable from '@/Components/ResultTable.vue';
 
 onMounted(() => {
     form.startDate = getToday()
@@ -30,6 +31,7 @@ const getData = async () => {
             data.data = res.data.data
             data.labels = res.data.labels
             data.totals = res.data.totals
+            data.type =res.data.type
             console.log(res.data);
         })
     } catch(e) {
@@ -54,29 +56,20 @@ const getData = async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="getData">
+                            分析方法<br>
+                            <input type="radio" v-model="form.type" value="perDay" checked><span class="mr-2">日別</span>
+                            <input type="radio" v-model="form.type" value="perMonth" ><span class="mr-2">月別</span>
+                            <input type="radio" v-model="form.type" value="perYear" ><span class="mr-2">年別</span>
+                            <input type="radio" v-model="form.type" value="decile" ><span class="mr-2">デシル分析</span>
+                            <br>
                             From: <input type="date" name="startDate" v-model="form.startDate">
                             To: <input type="date" name="endDate" v-model="form.endDate"><br>
                             <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
                         </form>
                         <div v-show="data.data">
                         <Chart :data="data"/>
+                        <ResultTable :data="data" />
                     </div>
-                        <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
-                            <table class="table-auto w-full text-left whitespace-no-wrap">
-                                <thead>
-                                <tr>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr v-for="item in data.data" :key="item.data">
-                                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.date }}</td>
-                                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.total }}</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
 
                     </div>
                 </div>
